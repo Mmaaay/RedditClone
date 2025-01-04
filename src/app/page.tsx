bf3eaf2eae6291866b43017a6db8c96a48d133ca
Mmaaay <mazenyousef.my@gmail.com>
2025-01-04 13:01:45 +0200

    changed main page, better logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,45 @@ export default async function Home() {
   return (
     <>
       <h1 className="font-bold text-3xl sm:text-4xl">Your Feed</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+      <div className="gap-y-4 md:gap-x-4 grid grid-cols-1 md:grid-cols-3 py-6">
         {/* @ts-expect-error server component*/}
         {session ? <CustomFeed /> : <GeneralFeed />}
         {/* subbreddit info */}
-        <div className="overflow-hiddem h-fit rounded-lg border border-gray-200 order-first md:order-last">
+        <div className="border-gray-200 order-first md:order-last border rounded-lg h-fit overflow-hiddem">
           <div className="bg-emerald-100 px-6 py-4">
-            <p className="font-semibold py-3 flex items-center gap-1.5">
+            <p className="flex items-center gap-1.5 py-3 font-semibold">
               <HomeIcon className="w-4 h-4" />
               Home
             </p>
           </div>
-          <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+          <div className="-my-3 px-6 py-4 divide-y divide-gray-100 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
               <p className="text-zinc-500">
                 Your Personal BreadIT homepage, Come here to check in with your
                 favourite communities
               </p>
             </div>
-            <Link
-              className={buttonVariants({ className: "w-full mt-4 mb-6" })}
-              href="/r/create"
-            >
-              Create Community
-            </Link>
+            {session ? (
+              <Link
+                className={buttonVariants({ className: "w-full mt-4 mb-6" })}
+                href="/r/create"
+              >
+                Create Community
+              </Link>
+            ) : (
+              <div> </div>
+            )}
+            <div className="mt-4">
+              <p className="mb-2 text-blue-600 text-center">
+                Log in to start your adventure.
+              </p>
+              <Link
+                className={buttonVariants({ className: "w-full" })}
+                href="/login"
+              >
+                Log In
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { CreateSubredditPayload } from "@/lib/validators/subreddit";
 import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
-import { CreateSubredditPayload } from "@/lib/validators/subreddit";
-import { toast } from "@/hooks/use-toast";
-import { useCustomToast } from "@/hooks/use-custom-toast";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const Page = () => {
-  const [input, setInput] = useState<string>("");
   const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/sign-in");
+    }
+  }, [status, router]);
+
+  const [input, setInput] = useState<string>("");
   const { logInToast } = useCustomToast();
 
   const { mutate: CreateCommunity, isLoading } = useMutation({
@@ -55,7 +64,7 @@ const Page = () => {
         variant: "default",
         action: (
           <Button
-            className="bg-green-200 text-bold text-black hover:bg-green-300 hover:text-black hover:shadow-md"
+            className="bg-green-200 hover:bg-green-300 hover:shadow-md text-black text-bold hover:text-black"
             onClick={() => router.push(`/r/${data}`)}
           >
             r/{data}
@@ -66,19 +75,19 @@ const Page = () => {
   });
 
   return (
-    <div className="container flex items-center h-full max-w-3xl mx-auto">
-      <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
+    <div className="flex items-center mx-auto max-w-3xl h-full container">
+      <div className="relative space-y-6 bg-white p-4 rounded-lg w-full h-fit">
         <div className="flex justify-between items-center">
-          <h1 className="text-xl font-semibold">Create a community</h1>
+          <h1 className="font-semibold text-xl">Create a community</h1>
         </div>
         <hr className="bg-zinc-500 h-px" />
         <div>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-xs pb-2">
+          <p className="font-medium text-lg">Name</p>
+          <p className="pb-2 text-xs">
             Community names including capitalization cannot be changed
           </p>
           <div className="relative">
-            <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
+            <p className="left-0 absolute inset-y-0 place-items-center grid w-8 text-sm text-zinc-400">
               r/
             </p>
             <Input
