c6581e91d46326a75320d4a6ffdac23370fb94ae
Mmaaay <mazenyousef.my@gmail.com>
2025-01-04 12:46:58 +0200

    changes schema, env

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -14,7 +14,9 @@ const Layout = async ({
   params: { slug: string };
 }) => {
   const session = await getAuthSession();
-
+  if (!session) {
+    return notFound();
+  }
   const subreddit = await db.subreddit.findFirst({
     where: { name: slug },
     include: {
@@ -57,19 +59,19 @@ const Layout = async ({
   });
 
   return (
-    <div className="sm:container max-w-7xl mx-auto h-full pt-12">
+    <div className="mx-auto pt-12 max-w-7xl h-full sm:container">
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-          <div className="flex flex-col col-span-2 space-y-6">{children}</div>
+        <div className="gap-y-4 md:gap-x-4 grid grid-cols-1 md:grid-cols-3 py-6">
+          <div className="flex flex-col space-y-6 col-span-2">{children}</div>
           {/* info Sidebar */}
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border-gray-200 order-first md:order-last">
+          <div className="md:block border-gray-200 order-first md:order-last hidden rounded-lg h-fit overflow-hidden">
             <div className="px-6 py-4">
-              <p className="font-semibold py-3">
+              <p className="py-3 font-semibold">
                 About our r/{subreddit?.name}
               </p>
             </div>
 
-            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
+            <dl className="bg-white px-6 py-4 divide-y divide-gray-100 text-sm leading-6">
               <div className="flex justify-between gap-x-4 py-3">
                 <dt className="text-gray-500">Created</dt>
                 <dd className="text-gray-700">
@@ -99,16 +101,17 @@ const Layout = async ({
                   subredditName={subreddit.name}
                 />
               ) : null}
-
-              <Link
-                className={buttonVariants({
-                  variant: "outline",
-                  className: "w-full mb-6",
-                })}
-                href={`/r/${slug}/submit`}
-              >
-                Create Post
-              </Link>
+              {isSubscribed ? (
+                <Link
+                  className={buttonVariants({
+                    variant: "outline",
+                    className: "w-full mb-6",
+                  })}
+                  href={`/r/${slug}/submit`}
+                >
+                  Create Post
+                </Link>
+              ) : null}
             </dl>
           </div>
         </div>
